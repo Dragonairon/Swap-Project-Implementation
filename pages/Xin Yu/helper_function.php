9b80a2e5caa7abf13e45fc33565c8cccc2602d07
School-work-account <2305373I@student.tp.edu.sg>
2026-02-01 13:03:10 +0800

    Updated helper_function.php

--- pages/Xin Yu/helper_function.php
+++ pages/Xin Yu/helper_function.php
@@ -1,7 +1,7 @@
 <?php
-// security function: regex whitelist function (I remember I did something like this, go and find it in the SWAP project)
+// security function: regex whitelist function
 function cleanUp($data) {
-    // regex syntax to search_Update: Found it: /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d@$!%*?&]{8,}$/
+    
     $pattern = "/[^a-zA-Z0-9\s\.,\!\?]/"; // /[^a-zA-Z0-9]/ format
 
     // remove "bad" characters/ sanitisation
